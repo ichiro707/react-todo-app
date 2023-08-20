f76a8a24f2ca2b2841f7dc734206d4a5794241da
ichiro707 <fw.root707@gmail.com>
2023-08-20 17:17:06 +0900

    TODOアプリの基本的な実装終了

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "iiii"]);
-  const [completeTodos, setCompleteTodos] = useState(["uuuu"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   // onChangeイベントを引数で受け取る
   // 入力された値をevent.target.valueで取得
@@ -19,6 +19,31 @@ export const App = () => {
     setIncompleteTodos(newTodos);
     // 入力欄に文字が残ってしまうため初期化
     setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // splice → 第一引数:何番目の要素から 第二引数:何個消すか
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
   };
 
   return (
@@ -36,15 +61,18 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {/* 2つめの引数 index には順番が入ってくる */}
+          {incompleteTodos.map((todo, index) => {
             return (
               // ループ内で返却している一番親のタグに key を設定する必要がある
               // 仮想DOMでは変更前と変更語を比較してレンダリングしているため何回目のループか
               // 正確に把握するために目印となる key を設定する
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                {/* 単純に引数でindexを渡すとページを読み込んだときに関数が呼ばれてしまうのでボタンを
+                クリックしたときに呼ばれるようにアロー関数にしている */}
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -53,19 +81,15 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               // keyのつけ忘れ注意
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
-          <div className="list-row">
-            <li>aaaaa</li>
-            <button>戻す</button>
-          </div>
         </ul>
       </div>
     </>
