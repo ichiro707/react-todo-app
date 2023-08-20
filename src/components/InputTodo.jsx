1cd906990897abf154ad7af2afa2bb70f4f3850b
ichiro707 <fw.root707@gmail.com>
2023-08-20 21:40:13 +0900

    一部だけStyleをコンポーネントが持つように変更

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,20 @@
 import React from "React";
 
+//JSXなので キャメルケースにする、値を文字列として扱う、「,」で区切る
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px",
+};
+
 export const InputTodo = (props) => {
   //受け取ったpropsを扱いやすように分割代入
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       {/* onChangeで input に変更があったことを検知 */}
       <input
         type="text"
